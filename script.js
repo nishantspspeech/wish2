4bb246815a477074efe4152aabbd24d95c3f43b8
nishantspspeech <nishantkumarpri@gmail.com>
2025-04-06 12:14:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const whatsappLink = document.getElementById("whatsappLink");
 
 input.addEventListener("input", () => {
   const name = input.value.trim();
-  const message = `🙏 *राम नवमी की ढेरों शुभकामनाएँ!* 🙏\n\nआपको और आपके परिवार को राम नवमी की हार्दिक शुभकामनाएं।\n\n🌸 जय श्री राम 🌸\n\nशुभकामनाएं: *${name}*`;
+  const imageUrl = "https://drive.google.com/file/d/1Jn9E50OxHfhQHn9DGeP3Jsyel08cTsgp/view?usp=sharing";
+
+  const message = `🙏 *राम नवमी की शुभकामनाएँ!* 🙏\n\nआपको और आपके परिवार को राम नवमी की हार्दिक शुभकामनाएं।\n\n🌸 जय श्री राम 🌸\n\nशुभकामनाएं: *${name}*\n\n📸 ${imageUrl}`;
 
   const encodedMsg = encodeURIComponent(message);
   whatsappLink.href = `https://wa.me/?text=${encodedMsg}`;
